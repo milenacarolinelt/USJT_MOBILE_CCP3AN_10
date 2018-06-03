da7c0accfa1f00f591ebc290e8f0ffa49a719d74
skyllaion <milena.carolinelt@hotmail.com>
2018-06-03 19:43:29 -0300

    Geodata em Ionic

---
+++ src/pages/pais/pais.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-pais',
+  templateUrl: 'pais.html',
+})
+export class PaisPage {
+  items;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.initializeItems();
+  }
+
+  initializeItems() {
+    this.items = [
+      'Amsterdam',
+      'Bogota',
+      'Buenos Aires',
+      'Cairo',
+      'Dhaka',
+      'Edinburgh',
+      'Geneva',
+      'Genoa',
+      'Glasglow',
+      'Hanoi',
+      'Hong Kong',
+      'Islamabad',
+      'Istanbul',
+      'Jakarta',
+      'Kiel',
+      'Kyoto',
+      'Le Havre',
+      'Lebanon',
+      'Lhasa',
+      'Lima',
+      'London',
+      'Los Angeles',
+      'Madrid',
+      'Manila',
+      'New York',
+      'Olympia',
+      'Oslo',
+      'Panama City',
+      'Peking',
+      'Philadelphia',
+      'San Francisco',
+      'Seoul',
+      'Taipeh',
+      'Tel Aviv',
+      'Tokio',
+      'Uelzen',
+      'Washington'
+    ];
+  }
+
+  getItems(ev) {
+    this.initializeItems();
+    var val = ev.target.value;
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PaisPage');
+  }
+
+}
